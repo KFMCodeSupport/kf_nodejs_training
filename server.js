6d53550678303f01caa8bfb4ca6fa259b1ecaaab
unknown <long.hoangngoc@kingfoodmart.net>
2023-06-25 11:56:35 +0700

    init

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http")
+
+const users = [
+    {
+      username: 'userA',
+      password: 'passwordA',
+      age: 25,
+      name: 'User A'
+    },
+    {
+      username: 'userB',
+      password: 'passwordB',
+      age: 30,
+      name: 'User B'
+    },
+    {
+      username: 'userC',
+      password: 'passwordC',
+      age: 35,
+      name: 'User C'
+    },
+    // Thêm người dùng khác tại đây nếu cần
+  ];
+
+  console.log(JSON.stringify(users));
+
+  app.listen(3000, () => {
+    log("Server is starting 3000")
+  })
