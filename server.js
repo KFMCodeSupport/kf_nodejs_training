b9841887d8b2281ed022e6e4d81ebe8fa266514d
unknown <long.hoangngoc@kingfoodmart.net>
2023-06-25 12:03:06 +0700

    init

--- server.js
+++ server.js
@@ -1,32 +1,13 @@
-const http = require("http")
+const http = require('http');
 
-const users = [
-    {
-      username: 'userA',
-      password: 'passwordA',
-      age: 25,
-      name: 'User A'
-    },
-    {
-      username: 'userB',
-      password: 'passwordB',
-      age: 30,
-      name: 'User B'
-    },
-    {
-      username: 'userC',
-      password: 'passwordC',
-      age: 35,
-      name: 'User C'
-    },
-    // Thêm người dùng khác tại đây nếu cần
-  ];
+// Tạo server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
 
-  console.log(JSON.stringify(users));
-
-
-  const app = http.createServer(processRequest)
-
-  app.listen(3000, () => {
-    log("Server is starting 3000")
-  })
+// Lắng nghe cổng 3000
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Server is running on http://127.0.0.1:3000/');
+});
