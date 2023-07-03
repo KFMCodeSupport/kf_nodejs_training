332e1c3016064313603244cac42d092fa403c7ca
unknown <long.hoangngoc@kingfoodmart.net>
2023-07-03 22:24:14 +0700

    init

--- Section 3.js
+++ Section 3.js
@@ -40,17 +40,34 @@
 // redirect http
 
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer(reqListener = (req,res) => {
     // console.log(req.url,req.method,req.headers);
     const url = req.url;
-    if (url == '/'){
+    const method = req.method;
+    if (url === '/'){
         res.write("<html>");
         res.write("<head><title>Enter Messager</title></head>");
         res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
         res.write("</html>");
         return res.end();
     }
+    if (url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+        });
+        fs.writeFileSync("message.txt","DUMMY");
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
+
     res.setHeader("Content-Type","text/html");
     res.write("<html>");
     res.write("<head><title>Application</title></head>");
